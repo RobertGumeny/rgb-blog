93a8f35131fe66bf5330f290c2b561cbcc2f064c
Robert Gumeny <robert.gumeny528@gmail.com>
2020-07-08 23:25:24 -0600

    blogs are displaying to main page

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="container">
     <h1>Robert's Blog</h1>
+    <div class="display-area">
+      <BlogPreview v-for="post in posts" :postData="post" :key="post.name" />
+    </div>
   </div>
 </template>
 
 <script>
+import BlogPreview from "../components/BlogPreview"
+import { getPosts } from '../api/posts'
 
 export default {
+  async asyncData() {
+    const posts = await getPosts();
+    return { posts: posts }
+  },
   components: {
+    BlogPreview
   }
 }
 </script>
@@ -15,6 +25,13 @@ export default {
 <style lang="scss" scoped>
 .container {
   padding: 80px 1.5em 0;
+}
+
+.display-area {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 4em;
+  margin-bottom: 1.5em;
 }
 
 h1 {
