347fba8295c90a8b974c5b573dc51c217a079ef4
Robert Gumeny <robert.gumeny528@gmail.com>
2020-09-23 15:28:57 -0600

    testing Netlify CMS

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,64 +1,60 @@
-
 export default {
   /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
-  mode: 'universal',
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "universal",
   /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
   head: {
     title: `Robert Gumeny`,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/style-resources'
+    "@nuxtjs/axios",
+    "@nuxtjs/style-resources",
+    "@nuxt/content"
   ],
 
   styleResources: {
-    scss: [
-      '~assets/scss/_colors.scss'
-    ]
+    scss: ["~assets/scss/_colors.scss"]
   },
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
   axios: {},
   /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
-  build: {
-  }
-}
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {}
+};
---
+++ pages/blog.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <li v-for="post of posts" :key="post.slug">
+      <nuxt-link :to="post.slug">{{ post.title }}</nuxt-link>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const posts = await $content("blog").fetch();
+
+    return {
+      posts
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding: 80px 1.5em 0;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
